492c88a36e03e405b9a826a8178bde94e8df0889
Purvibansal <54952529+Purvibansal@users.noreply.github.com>
2025-11-10 09:21:30 +0530

    Remove comments and clean up helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,5 @@
 public class helloworld {
-
-    // Your program begins with a call to main()
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
 }
